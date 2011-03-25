4cfadce9305856912dd239ca09ef96aa1caf73d1
Marco Wegner <devel@marcowegner.de>
2011-03-25 18:34:52 +0100

    Refactoring and extending the code

--- module/dot/src/main/java/fabric/module/dot/FabricDotGraphHandler.java
+++ module/dot/src/main/java/fabric/module/dot/FabricDotGraphHandler.java
@@ -3,7 +3,6 @@
  */
 package fabric.module.dot;
 
-import java.io.IOException;
 import java.util.Properties;
 
 import de.uniluebeck.sourcegen.Workspace;
@@ -159,62 +158,85 @@ public class FabricDotGraphHandler extends FabricDefaultHandler {
 
     @Override
     public void startTopLevelElement(FElement element) throws Exception {
-        createGraphNode(element.getID( ), element.getName( ), this.topLevelElementAttributes);
+        createGraphNode(element, this.topLevelElementAttributes);
     }
 
     @Override
     public void startLocalElement(FElement element, FComplexType parent) throws Exception {
-        createGraphEdge(
-                createGraphNode(element.getID( ), element.getName( ), this.localElementAttributes),
-                parent, null);
+        final DGraphNode node = createGraphNode(element, this.localElementAttributes);
+        createGraphEdge(parent, node, null);
     }
 
     @Override
     public void startTopLevelSimpleType(FSimpleType type, FElement parent) throws Exception {
-        final DGraphNode node = createGraphNode(type.getID( ), type.getName( ),
+        final DGraphNode node = getOrCreateGraphNode(type, type.getName( ),
                 this.topLevelSimpleTypeAttributes);
-        if (parent != null) {
-            createGraphEdge(node, parent, this.edgeTopLevelTypeReference);
-        }
+        createGraphEdge(parent, node, this.edgeTopLevelTypeReference);
     }
 
     @Override
     public void startLocalSimpleType(FSimpleType type, FElement parent) throws Exception {
-        createGraphEdge(
-                createGraphNode(type.getID( ), type.getName( ), this.localSimpleTypeAttributes),
-                parent, null);
+        final DGraphNode node = createGraphNode(type, this.localSimpleTypeAttributes);
+        createGraphEdge(parent, node, null);
     }
 
     @Override
-    public void startTopLevelComplexType(FComplexType type) throws Exception {
-        createGraphNode(type.getID( ), type.getName( ), this.topLevelComplexTypeAttributes);
+    public void startTopLevelComplexType(FComplexType type, FElement parent) throws Exception {
+        final DGraphNode node = getOrCreateGraphNode(type, type.getName( ),
+                this.topLevelComplexTypeAttributes);
+        createGraphEdge(parent, node, this.edgeTopLevelTypeReference);
     }
 
     @Override
     public void startLocalComplexType(FComplexType type, FElement parent) throws Exception {
-        createGraphEdge(
-                createGraphNode(type.getID( ), type.getName( ), this.localComplexTypeAttributes),
-                parent, null);
+        final DGraphNode node = createGraphNode(type, this.localComplexTypeAttributes);
+        createGraphEdge(parent, node, null);
     }
 
     /**
-     * Creates and returns a single node. This node represents either an element
-     * or a type. The newly created node is also added to the graph source file.
+     * <p>
+     * Creates and returns the corresponding graph node for the specified Schema
+     * object.
+     * </p>
+     * <p>
+     * This method does <b>not</b> check if a node with the specified ID already
+     * exists in the graph. If it is not apparent whether a node must be created
+     * or just needs to be retrieved, use
+     * {@link #getOrCreateGraphNode(FSchemaObject, String, String)} instead.
+     * </p>
+     * <p>
+     * The newly created node is also added to the graph source file.
+     * </p>
      * 
-     * @param id The new node's ID.
+     * @param object The Schema object for which the corresponding node is to be
+     *        created.
+     * @param attributes The node's attributes as comma-separated list.
+     * @return The newly created graph node.
+     */
+    private DGraphNode createGraphNode(FSchemaObject object, String attributes) {
+        final DGraphNode node = new DGraphNode(object.getID( ));
+        node.setLabel(createNodeLabel(object));
+        node.setNodeStyle(attributes);
+        this.graphSource.add(node);
+        return node;
+    }
+
+    /**
+     * Creates a corresponding graph node for the specified Schema object if,
+     * and only if, no such node currently exists in the graph. In this case the
+     * node is also added to the graph. If, on the other hand, the node does
+     * exist it is simply returned.
+     * 
+     * @param object The Schema object for which the corresponding node is to be
+     *        queried and/or created.
      * @param label The node's label.
      * @param attributes The node's attributes as comma-separated list.
      * @return The newly created graph node.
-     * @throws IOException If an error occurs while trying to write the node
-     *         data.
      */
-    private DGraphNode createGraphNode(int id, String label, String attributes) throws IOException {
-        DGraphNode node = this.graphSource.getNodeByID(id);
+    private DGraphNode getOrCreateGraphNode(FSchemaObject object, String label, String attributes) {
+        DGraphNode node = this.graphSource.getNodeByID(object.getID( ));
         if (node == null) {
-            node = new DGraphNode(id);
-            node.setLabel(label);
-            node.setNodeStyle(attributes);
-            this.graphSource.add(node);
+            node = createGraphNode(object, attributes);
         }
         return node;
     }
@@ -223,22 +245,94 @@ public class FabricDotGraphHandler extends FabricDefaultHandler {
      * Creates and returns an edge. The edge is also added to the graph source
      * file.
      * 
-     * @param node The target node.
-     * @param parent The parent object. The object's ID is used to retrieve the
-     *        corresponding node which will function as the edge's source node.
+     * @param source The new edge's source node.
+     * @param target The new edge's target node.
      * @param attributes The edge's attributes as comma-separated list.
      * @return The newly created graph edge.
-     * @throws IOException If an error occurs while trying to write the node
-     *         data.
      */
-    private DGraphEdge createGraphEdge(DGraphNode node, FSchemaObject parent, String attributes)
-            throws IOException {
-        // retrieve the parent object's node
-        final DGraphNode parentNode = this.graphSource.getNodeByID(parent.getID( ));
-        // create the edge between the two nodes
-        final DGraphEdge edge = new DGraphEdge(parentNode, node);
+    private DGraphEdge createGraphEdge(DGraphNode source, DGraphNode target, String attributes) {
+        final DGraphEdge edge = new DGraphEdge(source, target);
         edge.setLineStyle(attributes);
         this.graphSource.add(edge);
         return edge;
+    }
+
+    /**
+     * Creates and returns an edge. The edge is also added to the graph source
+     * file.
+     * 
+     * @param object The parent Schema object. That object's ID is used to
+     *        retrieve the corresponding graph node which will function as the
+     *        new edge's source node.
+     * @param target The new edge's target node.
+     * @param attributes The edge's attributes as comma-separated list.
+     * @return The newly created graph edge.
+     */
+    private DGraphEdge createGraphEdge(FSchemaObject object, DGraphNode target, String attributes) {
+        final DGraphNode source = this.graphSource.getNodeByID(object.getID( ));
+        return createGraphEdge(source, target, attributes);
+    }
+
+    /**
+     * Creates the label for a node. This label is used in the graph to give the
+     * node a more descriptive text.
+     * 
+     * @param object The Schema object to create the label for.
+     * @return The node's new label.
+     */
+    private String createNodeLabel(FSchemaObject object) {
+        final String label;
+        if (object instanceof FElement) {
+            label = createElementLabel((FElement)object);
+        } else if (object instanceof FSimpleType) {
+            label = createSimpleTypeLabel((FSimpleType)object);
+        } else if (object instanceof FComplexType) {
+            label = createComplexTypeLabel((FComplexType)object);
+        } else {
+            label = null;
+        }
+        return label;
+    }
+
+    /**
+     * Creates the label for an element node.
+     * 
+     * @param e The Schema element to create a label for.
+     * @return The element node's new label.
+     */
+    private String createElementLabel(FElement e) {
+        return e.isReference( ) ? "<<reference>>" : e.getName( );
+    }
+
+    /**
+     * Creates the label for a simple type node.
+     * 
+     * @param type The Schema simple type to create the label for.
+     * @return The type node's new label.
+     */
+    private String createSimpleTypeLabel(FSimpleType type) {
+        final String label;
+        if (type.isTopLevel( )) {
+            label = type.getName( );
+        } else {
+            label = null;
+        }
+        return label;
+    }
+
+    /**
+     * Creates the label for a complex type node.
+     * 
+     * @param type The Schema complex type to create the label for.
+     * @return The type node's new label.
+     */
+    private String createComplexTypeLabel(FComplexType type) {
+        final String label;
+        if (type.isTopLevel( )) {
+            label = type.getName( );
+        } else {
+            label = null;
+        }
+        return label;
     }
 }
