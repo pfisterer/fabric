f1f333c4b23524ab7ee42119d9ac8b3f8821f3a3
Joss Widderich <widderich@itm.uni-luebeck.de>
2011-08-17 13:56:50 +0200

    XSD Testfälle zu XML und JAVA

---
+++ junit/src/test/java/Waehrung2.java
@@ -0,0 +1,38 @@
+/*
+ * Test case for XSD attributes
+ */
+
+
+package XSD2JavaTestCases.JavaFiles;
+import java.lang.String;
+
+import org.simpleframework.xml.Attribute;
+import org.simpleframework.xml.Root;
+
+@Root
+public class Waehrung2{
+	
+	@Attribute
+	private String name;
+	
+	@Attribute
+	private String waehrungsCode;
+	
+	public Waehrung2() {
+	      super();
+	} 
+	
+	public Waehrung2(String name, String waehrungsCode) {
+	      this.name = name;
+	      this.waehrungsCode = waehrungsCode;
+	}
+	
+	public String getName(){
+		return this.name;
+	}
+	
+	public String getWaehrungsCode(){
+		return this.waehrungsCode;
+	}
+		
+}
