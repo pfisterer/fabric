0ec3b480cb1e3f0e1f9e737da1cdefb3c8ef8f15
Sascha Seidel <seidels@informatik.uni-luebeck.de>
2011-08-25 01:23:04 +0200

    Please don't commit broken code to master branch. Create your own branch for development instead.

---
+++ junit/src/test/java/fabric/testsuite/java/Waehrung3JSF.java
@@ -0,0 +1,56 @@
+//package fabric.testsuite.java;
+//
+//import de.uniluebeck.sourcegen.java.JSourceFileImpl;
+//import fabric.module.typegen.AttributeContainer;
+//
+///**
+// * Created by IntelliJ IDEA.
+// * User: reichart
+// * Date: 22.08.11
+// * Time: 12:07
+// * To change this template use File | Settings | File Templates.
+// */
+//public class Waehrung3JSF {
+//    /**
+//     * JSourceFile
+//     */
+//    private JSourceFileImpl waehrung3Java;
+//
+//    /**
+//     * Name of XSD root element
+//     */
+//    private static final String ROOT        = "Waehrung3";
+//
+//    /**
+//     * Java file ending
+//     */
+//    private static final String ENDING_JAVA = ".java";
+//
+//    /**
+//     * Constructor
+//     */
+//    public Waehrung3JSF() {
+//        buildWaehrung3Java();
+//    }
+//
+//    /**
+//     * Generates the expected JSourceFile for Waehrung3.java
+//     */
+//    private void buildWaehrung3Java() {
+//        AttributeContainer waehrung3Container = AttributeContainer.newBuilder()
+//                                           .setName(ROOT)
+//                                           .addElement("String", "Name")
+//                                           .addAttribute("String", "Waehrungscode")
+//                                           .build();
+//        waehrung3Java = new JSourceFileImpl(ROOT.toLowerCase(), ROOT + ENDING_JAVA);
+//        waehrung3Java.add(waehrung3Container.asJClass());
+//    }
+//
+//    /**
+//     * @return JSourceFile of Waehrung3.java
+//     */
+//    public JSourceFileImpl getWaehrung3Java() {
+//        return waehrung3Java;
+//    }
+//
+//}
--- module/typegen/src/main/java/fabric/module/typegen/FabricTypeGenHandler.java
+++ module/typegen/src/main/java/fabric/module/typegen/FabricTypeGenHandler.java
@@ -63,152 +63,152 @@ public class FabricTypeGenHandler extends FabricDefaultHandler
         }
     }
 
-    @Override
-    public void startSchema(FSchema schema) throws Exception {
-        typeGen.generateRootContainer();
-        /**
-        AttributeContainer root = AttributeContainer.newBuilder()
-                                    .setName(ROOT)
-                                    .build();
-        containers.push(root);
-         */
-    }
-
-    @Override
-    public void endSchema(FSchema schema) throws Exception {
-        typeGen.generateRootClass();
-        /**
-        AttributeContainer root = containers.pop();
-        root.asClassObject();
-         */
-    }
-
-    @Override
-    public void startTopLevelElement(FElement element) throws Exception {
-        /**
-         * Nothing to do.
-         */
-    }
-
-    @Override
-    public void endTopLevelElement(FElement element) {
-        /**
-         * Nothing to do.
-         */
-    }
-
-    @Override
-    public void startTopLevelSimpleType(FSimpleType type, FElement parent) throws Exception {
-        typeGen.addSimpleType();
-        /**
-         containers.push(containers.pop().toBuilder()
-                            .addElement("Hier muss der Typ hin (siehe Mapper)", type.getName())
-                            .build());
-         */
-
-        /**
-         * Der SimpleType muss als Variable der Main-Klasse hinzugefügt werden.
-         * typegen.addAttributeToMain(type);
-         *
-         * in Java: mittels des AttributeContainer-Objekts der Main-Klasse
-         *
-         * Im TypeGen muss auf den Language-Mapper zurückgegriffen werden!
-         */
-    }
-
-    @Override
-    public void endTopLevelSimpleType(FSimpleType type, FElement parent) {
-        /**
-         * Nothing to do.
-         */
-    }
-
-    @Override
-    public void startLocalSimpleType(FSimpleType type, FElement parent) throws Exception {
-        typeGen.addSimpleType();
-
-        /**
-         * Der SimpleType muss als Variable der aktuellen Klasse hinzugefügt werden.
-         * typegen.addAttribute(type);
-         *
-         * in Java: mittels des aktuellen AttributeContainer-Objekts
-         *
-         * Im TypeGen muss auf den Language-Mapper zurückgegriffen werden!
-         */
-    }
-
-    @Override
-    public void endLocalSimpleType(FSimpleType type, FElement parent) {
-        /**
-         * Nothing to do.
-         */
-    }
-
-    @Override
-    public void startTopLevelComplexType(FComplexType type, FElement parent) throws Exception {
-
-        /**
-         * Für den ComplexType muss eine neue Klasse in TypeGen erstellt werden. Diese wird
-         * der Liste der noch nicht vollständigen Klassen hinzugefügt. Außerdem muss ein Objekt
-         * dieser Klasse in der aktuellen Klasse hinzugefügt werden.
-         * typegen.generateClass(type);
-         * typegen.addClassToParent();
-         *
-         * in Java: mittels eines neuen AttributeContainer-Objekts
-         */
-    }
-
-    @Override
-    public void endTopLevelComplexType(FComplexType type, FElement parent) {
-        /**
-         * Die Klasse wird aus der Liste der noch nicht vollständigen Klassen entfernt.
-         * Das SourceFile der Klasse muss im Workspace erstellt werden.
-         * typegen.generateSourceFile();
-         * typegen.removeClassFromList();
-         *
-         * in Java: JSourceFile mittels des aktuellen AttributeContainer-Objekts erstellen
-         */
-    }
-
-    @Override
-    public void startLocalElement(FElement element, FComplexType parent) throws Exception {
-        /**
-         * Hier muss nichts gemacht werden.
-         */
-    }
-
-    @Override
-    public void endLocalElement(FElement element, FComplexType parent) {
-        /**
-         * Hier muss nichts gemacht werden.
-         */
-    }
-
-    @Override
-    public void startLocalComplexType(FComplexType type, FElement parent) throws Exception {
-        /**
-         * Siehe startTopLevelComplexType --> in private Funktion auslagern!
-         */
-    }
-
-    @Override
-    public void endLocalComplexType(FComplexType type, FElement parent) {
-        /**
-         * Siehe startTopLevelComplexType --> in private Funktion auslagern!
-         */
-    }
-
-    @Override
-    public void startElementReference(FElement element) throws Exception {
-        /**
-         * Was ist das?
-         */
-    }
-
-    @Override
-    public void endElementReference(FElement element) throws Exception {
-        /**
-         * Was ist das?
-         */
-    }
+//    @Override
+//    public void startSchema(FSchema schema) throws Exception {
+//        typeGen.generateRootContainer();
+//        /**
+//        AttributeContainer root = AttributeContainer.newBuilder()
+//                                    .setName(ROOT)
+//                                    .build();
+//        containers.push(root);
+//         */
+//    }
+//
+//    @Override
+//    public void endSchema(FSchema schema) throws Exception {
+//        typeGen.generateRootClass();
+//        /**
+//        AttributeContainer root = containers.pop();
+//        root.asClassObject();
+//         */
+//    }
+//
+//    @Override
+//    public void startTopLevelElement(FElement element) throws Exception {
+//        /**
+//         * Nothing to do.
+//         */
+//    }
+//
+//    @Override
+//    public void endTopLevelElement(FElement element) {
+//        /**
+//         * Nothing to do.
+//         */
+//    }
+//
+//    @Override
+//    public void startTopLevelSimpleType(FSimpleType type, FElement parent) throws Exception {
+//        typeGen.addSimpleType();
+//        /**
+//         containers.push(containers.pop().toBuilder()
+//                            .addElement("Hier muss der Typ hin (siehe Mapper)", type.getName())
+//                            .build());
+//         */
+//
+//        /**
+//         * Der SimpleType muss als Variable der Main-Klasse hinzugefügt werden.
+//         * typegen.addAttributeToMain(type);
+//         *
+//         * in Java: mittels des AttributeContainer-Objekts der Main-Klasse
+//         *
+//         * Im TypeGen muss auf den Language-Mapper zurückgegriffen werden!
+//         */
+//    }
+//
+//    @Override
+//    public void endTopLevelSimpleType(FSimpleType type, FElement parent) {
+//        /**
+//         * Nothing to do.
+//         */
+//    }
+//
+//    @Override
+//    public void startLocalSimpleType(FSimpleType type, FElement parent) throws Exception {
+//        typeGen.addSimpleType();
+//
+//        /**
+//         * Der SimpleType muss als Variable der aktuellen Klasse hinzugefügt werden.
+//         * typegen.addAttribute(type);
+//         *
+//         * in Java: mittels des aktuellen AttributeContainer-Objekts
+//         *
+//         * Im TypeGen muss auf den Language-Mapper zurückgegriffen werden!
+//         */
+//    }
+//
+//    @Override
+//    public void endLocalSimpleType(FSimpleType type, FElement parent) {
+//        /**
+//         * Nothing to do.
+//         */
+//    }
+//
+//    @Override
+//    public void startTopLevelComplexType(FComplexType type, FElement parent) throws Exception {
+//
+//        /**
+//         * Für den ComplexType muss eine neue Klasse in TypeGen erstellt werden. Diese wird
+//         * der Liste der noch nicht vollständigen Klassen hinzugefügt. Außerdem muss ein Objekt
+//         * dieser Klasse in der aktuellen Klasse hinzugefügt werden.
+//         * typegen.generateClass(type);
+//         * typegen.addClassToParent();
+//         *
+//         * in Java: mittels eines neuen AttributeContainer-Objekts
+//         */
+//    }
+//
+//    @Override
+//    public void endTopLevelComplexType(FComplexType type, FElement parent) {
+//        /**
+//         * Die Klasse wird aus der Liste der noch nicht vollständigen Klassen entfernt.
+//         * Das SourceFile der Klasse muss im Workspace erstellt werden.
+//         * typegen.generateSourceFile();
+//         * typegen.removeClassFromList();
+//         *
+//         * in Java: JSourceFile mittels des aktuellen AttributeContainer-Objekts erstellen
+//         */
+//    }
+//
+//    @Override
+//    public void startLocalElement(FElement element, FComplexType parent) throws Exception {
+//        /**
+//         * Hier muss nichts gemacht werden.
+//         */
+//    }
+//
+//    @Override
+//    public void endLocalElement(FElement element, FComplexType parent) {
+//        /**
+//         * Hier muss nichts gemacht werden.
+//         */
+//    }
+//
+//    @Override
+//    public void startLocalComplexType(FComplexType type, FElement parent) throws Exception {
+//        /**
+//         * Siehe startTopLevelComplexType --> in private Funktion auslagern!
+//         */
+//    }
+//
+//    @Override
+//    public void endLocalComplexType(FComplexType type, FElement parent) {
+//        /**
+//         * Siehe startTopLevelComplexType --> in private Funktion auslagern!
+//         */
+//    }
+//
+//    @Override
+//    public void startElementReference(FElement element) throws Exception {
+//        /**
+//         * Was ist das?
+//         */
+//    }
+//
+//    @Override
+//    public void endElementReference(FElement element) throws Exception {
+//        /**
+//         * Was ist das?
+//         */
+//    }
 }
